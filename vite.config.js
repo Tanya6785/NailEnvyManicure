028b6443721fd5e0758120e07c1b46f749b4360b
NailEnvy <166920460+Tanya6785@users.noreply.github.com>
2025-05-26 17:32:39 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NailEnvyManicure/',  // <-- добавь эту строку
+  base: '/NailEnvyManicure/',  // <-- название твоего репозитория с слешами
   plugins: [react()],
 })
-
